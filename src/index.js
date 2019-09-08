07c06250baaf54f993ab974189aba34b25dc5def
Daniel Kim <chipperday@gmail.com>
2019-09-08 13:47:41 -0700

    changed bg

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        background: "pink"
+      }}
+    >
       <h1>Hello DK v.2</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
