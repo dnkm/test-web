bd5cae4f473cd19c5d56f030ab278a55400a3d85
Daniel Kim <chipperday@gmail.com>
2019-09-08 13:45:23 -0700

    updated version number

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello DK</h1>
+      <h1>Hello DK v.2</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
